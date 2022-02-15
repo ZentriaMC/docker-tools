9a23386b74d201a7d8c6d395b7c51499587072d1
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-02-15 18:17:33 +0200

    Allow specifying target directory

--- flake.nix
+++ flake.nix
@@ -12,42 +12,42 @@
       # being used in extraCommands.
       # This is based on dockerTools.shadowSetup, except it does not require
       # runAsRoot
-      shadowSetup = { runtimeShell ? "/bin/sh" }: ''
-        mkdir -p etc/pam.d
-        if [[ ! -f etc/passwd ]]; then
-          echo "root:x:0:0::/root:${runtimeShell}" > etc/passwd
-          echo "root:!x:::::::" > etc/shadow
+      shadowSetup = { runtimeShell ? "/bin/sh", targetDir ? "." }: ''
+        mkdir -p ${targetDir}/etc/pam.d
+        if [[ ! -f ${targetDir}/etc/passwd ]]; then
+          echo "root:x:0:0::/root:${runtimeShell}" > ${targetDir}/etc/passwd
+          echo "root:!x:::::::" > ${targetDir}/etc/shadow
         fi
-        if [[ ! -f etc/group ]]; then
-          echo "root:x:0:" > etc/group
-          echo "root:x::" > etc/gshadow
+        if [[ ! -f ${targetDir}/etc/group ]]; then
+          echo "root:x:0:" > ${targetDir}/etc/group
+          echo "root:x::" > ${targetDir}/etc/gshadow
         fi
-        if [[ ! -f etc/pam.d/other ]]; then
-          cat > etc/pam.d/other <<EOF
+        if [[ ! -f ${targetDir}/etc/pam.d/other ]]; then
+          cat > ${targetDir}/etc/pam.d/other <<EOF
         account sufficient pam_unix.so
         auth sufficient pam_rootok.so
         password requisite pam_unix.so nullok sha512
         session required pam_unix.so
         EOF
         fi
-        if [[ ! -f etc/login.defs ]]; then
-          touch etc/login.defs
+        if [[ ! -f ${targetDir}/etc/login.defs ]]; then
+          touch ${targetDir}/etc/login.defs
         fi
 
-        chmod 640 etc/gshadow
-        chmod 640 etc/shadow
-        chmod 644 etc/passwd
-        chmod 644 etc/group
+        chmod 640 ${targetDir}/etc/gshadow
+        chmod 640 ${targetDir}/etc/shadow
+        chmod 644 ${targetDir}/etc/passwd
+        chmod 644 ${targetDir}/etc/group
       '';
 
       # Symlinks CA certs into place for HTTPS etc. via curl, java and other
       # programs to work.
-      symlinkCACerts = { cacert }: ''
-        mkdir -p etc/ssl/certs etc/pki/tls/certs
-        ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt etc/ssl/certs/ca-bundle.crt
-        ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt etc/ssl/certs/ca-certificates.crt
-        ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt etc/pki/tls/certs/ca-bundle.crt
-        ln -s ${cacert.p11kit}/etc/ssl/trust-source etc/ssl/trust-source
+      symlinkCACerts = { cacert, targetDir ? "." }: ''
+        mkdir -p ${targetDir}/etc/ssl/certs ${targetDir}/etc/pki/tls/certs
+        ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt ${targetDir}/etc/ssl/certs/ca-bundle.crt
+        ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt ${targetDir}/etc/ssl/certs/ca-certificates.crt
+        ln -s ${cacert}/etc/ssl/certs/ca-bundle.crt ${targetDir}/etc/pki/tls/certs/ca-bundle.crt
+        ln -s ${cacert.p11kit}/etc/ssl/trust-source ${targetDir}/etc/ssl/trust-source
       '';
 
       # Builds environment. output is output name of derivation, pkgs is instance of imported nixpkgs, pkgsList is a list of derivations.
@@ -66,11 +66,11 @@
           paths;
 
       # Creates a FHS environment symlink script.
-      setupFHSScript = { pkgs, paths }@args:
+      setupFHSScript = { pkgs, paths, targetDir ? "." }@args:
         let
           fhsPaths = self.lib.setupFHS args;
         in
-        toString (nixpkgs.lib.mapAttrsToList (name: env: "ln -s ${env}/${name} ${name};") fhsPaths);
+        toString (nixpkgs.lib.mapAttrsToList (name: env: "ln -s ${env}/${name} ${targetDir}/${name};") fhsPaths);
 
       dockerConfig = {
         # Turns { TZ = "UTC"; LANG = "C"; } into [ "TZ=UTC", "LANG=C" ]
